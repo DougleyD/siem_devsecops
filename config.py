4ed08d14b21cbb1ccc4300e1da13fed12b0fccad
Daniel Mendes <danielmcorreia03@gmail.com>
2025-04-12 17:30:33 -0300

    Validação de e-mail e inclusão de ícones no input.

--- config.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-class Config:
-    SECRET_KEY = f"{os.getenv('SECRET')}"
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
