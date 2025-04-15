2ccb6db818008334f568c843040053915ff801e7
Daniel Mendes <danielmcorreia03@gmail.com>
2025-04-14 21:27:49 -0300

    Implementação de 2FA com PyOTP.

--- app/models/users.py
+++ app/models/users.py
@@ -1,3 +1,4 @@
+# models/users.py
 from app import db
 from werkzeug.security import generate_password_hash
 
@@ -6,6 +7,9 @@ class User(db.Model):
     username = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
+    tfa_secret = db.Column(db.String(255))
+    tfa_enabled = db.Column(db.Boolean, default=False)
+    tfa_expiration = db.Column(db.DateTime)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
