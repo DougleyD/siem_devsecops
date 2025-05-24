9dce78d175a58175864cd2263436269d56384b41
Daniel Mendes <danielmcorreia03@gmail.com>
2025-05-24 15:53:37 -0300

    Implementando sessão

--- app/controllers/main_controller.py
+++ app/controllers/main_controller.py
@@ -1,4 +1,7 @@
+from datetime import datetime
 from flask import Blueprint, redirect, render_template, url_for
+
+from app.services.auth_service import login_required
 
 main_bp = Blueprint('main', __name__)
 
@@ -7,5 +10,6 @@ def root():
    return redirect(url_for('auth.login'))
 
 @main_bp.route('/dashboard', methods=['GET'])
-def dashboard():
-   return render_template('application/dashboard.html', title='EventTrace | Dashboard')
+@login_required
+def dashboard(user):
+   return render_template('application/dashboard.html', title='EventTrace | Dashboard', user=user)
