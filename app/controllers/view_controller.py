fead053552cbda2c0076a45dfcdb167e23e920b3
Daniel Mendes <danielmcorreia03@gmail.com>
2025-05-31 11:50:50 -0300

    Download do agent pelo dashboard

--- app/controllers/view_controller.py
+++ app/controllers/view_controller.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, redirect, render_template, url_for
-
+from flask import Blueprint, redirect, render_template, url_for, send_file, current_app
+from app.services.auth_service import login_required
+import os
+   
 from app.services.auth_service import login_required
 
 view_bp = Blueprint('view', __name__)
@@ -8,3 +10,25 @@ view_bp = Blueprint('view', __name__)
 @login_required
 def events(user):
    return render_template('application/events.html', title='EventTrace | Event View', user=user)
+
+@view_bp.route('/download', methods=['GET'])
+@login_required
+def download(user):
+    try:
+        # Caminho para o arquivo que será disponibilizado para download
+        file_path = os.path.join(current_app.root_path, 'static', 'agent', 'agent.py')
+        
+        # Verifica se o arquivo existe
+        if not os.path.exists(file_path):
+            raise FileNotFoundError("Arquivo não encontrado")
+            
+        # Envia o arquivo como anexo para download
+        return send_file(
+            file_path,
+            as_attachment=True,
+            download_name="agent.py",  # Nome que aparecerá no download
+            mimetype='application/octet-stream'
+        )
+    except Exception as e:
+        current_app.logger.error(f"Erro ao fazer download: {str(e)}")
+        return redirect(url_for('view.error_page'))
